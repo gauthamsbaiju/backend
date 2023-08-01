10879ffd9492d998d10305d9c9afe37f89cdb250
gauthamsbaiju <gauthamsbaiju.mec@gmail.com>
2023-08-01 18:26:25 +0530

    Typescript file added

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const server = express();
+
+server.get('/*', (req,res)=>{
+    console.log(req.url);
+    res.status(200).send("Hello world typescript");
+})
+
+server.listen(3000, ()=>{
+    console.log("Server is listening to port 3000");
+})
+
+
+
